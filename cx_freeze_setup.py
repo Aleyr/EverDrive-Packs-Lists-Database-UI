40f26e6e5730f3dbc23ddf9745bb6e6ba8a9ab24
Aleyr Lengiac <aleyr7@gmail.com>
2018-11-14 14:25:51 -0600

    Added details on the cx_freeze setup.py file.

--- cx_freeze_setup.py
+++ cx_freeze_setup.py
@@ -1,7 +1,18 @@
+import sys
 import utils
 from cx_Freeze import setup, Executable
 
-setup(name = utils.APP_NAME,
-      version = utils.VERSION,
-      description = "SmokeMonster everdrive pack list scripts UI",
-      executables = [Executable("SmokeMonster-packs-UI.py")])
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["py2app"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = utils.APP_NAME,
+        version = utils.VERSION,
+        description = "SmokeMonster everdrive pack list scripts UI",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("SmokeMonster-packs-UI.py", base=base)])
